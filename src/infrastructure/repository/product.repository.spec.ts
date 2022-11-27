ca95950a27ee881f11d95cabb893bab08e0e4069
wallisonmoura <wallison_cedaspy@hotmail.com>
2022-11-27 01:29:29 -0300

    implementation of the infrastructure layer, repository and tests

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize-typescript"
+import Product from "../../domain/entity/product"
+import ProductModel from "../db/sequelize/model/product.model"
+import ProductRepository from "./product.repository"
 
 describe('Product repository test', () => {
 
@@ -11,9 +14,83 @@ describe('Product repository test', () => {
       logging: false,
       sync: { force: true}
     })
+    sequelize.addModels([ProductModel])
+    await sequelize.sync()
+    
+  })
 
-    afterEach(async () => {
-      await sequelize.close()
+  afterEach(async () => {
+    await sequelize.close()
+  })
+
+  it('Should create a product', async () => {
+    const productRepository = new ProductRepository()
+    const product = new Product('1', 'product 1', 100)
+
+    await productRepository.create(product)
+
+    const productModel = await ProductModel.findOne({ where: { id: '1'}})
+    expect(productModel.toJSON()).toStrictEqual({
+      id: '1',
+      name: 'product 1',
+      price: 100
     })
+  })
+
+  it('Should update a product', async () => {
+    const productRepository = new ProductRepository()
+    const product = new Product('1', 'product 1', 100)
+
+    await productRepository.create(product)
+
+    const productModel = await ProductModel.findOne({ where: { id: '1'}})
+    expect(productModel.toJSON()).toStrictEqual({
+      id: '1',
+      name: 'product 1',
+      price: 100
+    })
+
+    product.changeName('product 2')
+    product.changePrice(200)
+
+    await productRepository.update(product)
+
+    const productModel2 =  await ProductModel.findOne({ where: { id: '1'}})
+    expect(productModel2.toJSON()).toStrictEqual({
+      id: '1',
+      name: 'product 2',
+      price: 200
+    })
+  })
+
+  it('Should find a product', async () => {
+    const productRepository = new ProductRepository()
+    const product = new Product('1', 'product 1', 100)
+
+    await productRepository.create(product)
+
+    const productModel = await ProductModel.findOne({ where: { id: '1'}})
+    
+    const foundProduct = await productRepository.find('1')
+    
+    expect(productModel.toJSON()).toStrictEqual({
+      id: foundProduct.id,
+      name: foundProduct.name,
+      price: foundProduct.price
+    })
+  })
+
+  it('Should find all products', async () => {
+    const productRepository = new ProductRepository()
+    const product = new Product('1', 'product 1', 100)
+    await productRepository.create(product)
+
+    const product2 = new Product('2', 'product 2', 200)
+    await productRepository.create(product2)
+
+    const foundProducts = await productRepository.findAll()
+    const products = [product, product2]
+
+    expect(products).toEqual(foundProducts)
   })
 })
