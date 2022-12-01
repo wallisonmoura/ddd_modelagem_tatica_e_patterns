a626285d07b166a90457b736ea53c0aec687cdb9
wallisonmoura <wallison_cedaspy@hotmail.com>
2022-12-01 18:45:18 -0300

    implementation of the repository, model and test of order in infrastructure

--- src/infrastructure/repository/customer.repository.spec.ts
+++ src/infrastructure/repository/customer.repository.spec.ts
@@ -4,7 +4,7 @@ import Customer from "../../domain/entity/customer";
 import CustomerModel from "../db/sequelize/model/customer.model";
 import CustomerRepository from "./customer.repository";
 
-describe('', () => {
+describe('Customer repository test', () => {
   let sequelize: Sequelize;
 
   beforeEach(async () => {
