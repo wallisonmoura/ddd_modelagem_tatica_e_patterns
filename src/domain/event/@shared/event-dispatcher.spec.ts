484c9736fefa51454d968fb724d0cf5877b0f56d
wallisonmoura <wallison_cedaspy@hotmail.com>
2022-12-01 23:17:36 -0300

    implementation of events

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,17 @@
+import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler"
+import EventDispatcher from "./event-dispatcher"
+
+describe('Domain events tests', () => {
+
+  it('Should register an event handler', () => {
+
+    const eventDispatcher = new EventDispatcher()
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler)
+
+    expect(eventDispatcher.getEventHandlers['ProductCreatedEvent']).toBeDefined()
+    expect(eventDispatcher.getEventHandlers['ProductCreatedEvent'].length).toBe(1)
+    expect(eventDispatcher.getEventHandlers['ProductCreatedEvent'][0]).toMatchObject(eventHandler)
+  })
+})
